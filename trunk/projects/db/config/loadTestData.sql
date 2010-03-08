595786b01597364519db95503227b3b41f8c6a2b
suresh.ghuge <suresh.ghuge@be54feb2-af79-11de-9f64-4bfc55a448e5>
2010-03-08 12:40:54 +0000

    Merged from changes branch to trunk for 1.0.2 ALPHA release

---
+++ trunk/projects/db/config/loadTestData.sql
@@ -0,0 +1,5 @@
+-- Creating Test Data
+INSERT INTO  TestEmpTable(fname, lname ,dept) VALUES('kiran' , 'pawar' , 12);
+INSERT INTO  TestEmpTable(fname, lname ,dept) VALUES('pankaj' , 'chawhan' , 11);
+INSERT INTO  TestEmpTable(fname, lname ,dept) VALUES('rakesh' , 'shete' , 13);
+INSERT INTO  TestEmpTable(fname, lname ,dept) VALUES('suresh' , 'ghuge' , 14);
