12627a1631cf89f86013b4b4b59294c2ce91d2ba
Suresh Ghuge <suresh.ghuge@gmail.com>
2010-07-05 10:14:20 +0000

    modified for code clean-up and inline comments

--- projects/DBUtility/src/com/google/db/util/DBUtilMain.java
+++ projects/DBUtility/src/com/google/db/util/DBUtilMain.java
@@ -1,4 +1,4 @@
-//Copyright 2009 Google Inc.
+//Copyright 2010 Google Inc.
 //
 //Licensed under the Apache License, Version 2.0 (the "License");
 //you may not use this file except in compliance with the License.
@@ -22,26 +22,52 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * This class provide main method to run this utility. User need to supply
+ * firstName lastName deptID emplNo and path of file to be uploaded as command
+ * line argument. e.g Ramesh Rane 4 8 "C:\Documents and
+ * Settings\abc\Desktop\abc.doc".
+ * 
+ * @author suresh_ghuge
+ */
 public class DBUtilMain {
+	/**
+	 * main method.
+	 * 
+	 * @param args
+	 * @throws IOException throws IOException if input file is not found or
+	 *             corrupted
+	 */
 	public static void main(String[] args) throws IOException {
 		Employee emp1 = new Employee();
 
+		// set employee first name
 		emp1.setFirstName(args[0]);
+		// set employee last name
 		emp1.setLastName(args[1]);
+		// parse and set department id
 		emp1.setDeptId(Integer.parseInt(args[2]));
+		// parse and set employee id
 		emp1.setEmpId(Integer.parseInt(args[3]));
 
 		DBUtilMain main = new DBUtilMain();
-
+		// create File object from given file path.
 		File reportFile = new File(args[4]);
+		// create InputStream for given file
 		InputStream is = new FileInputStream(reportFile);
 		int length = (int) reportFile.length();
+		// convert InputStream into array of byte
 		byte[] report = getBytes(length, is);
 		emp1.setReport(report);
-
+		// save employee object
 		main.saveEmployee(emp1);
 	}
 
+	/**
+	 * This method will persist an employee object in underlying database.
+	 * 
+	 * @param emp1 Employee object
+	 */
 	private void saveEmployee(Employee emp1) {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction trx = session.beginTransaction();
@@ -49,43 +75,11 @@ public class DBUtilMain {
 		trx.commit();
 	}
 
-	// Returns the contents of the file in a byte array.
-	public static byte[] getBytesFromFile(File file) throws IOException {
-		InputStream is = new FileInputStream(file);
-
-		// Get the size of the file
-		long length = file.length();
-
-		// You cannot create an array using a long type.
-		// It needs to be an int type.
-		// Before converting to an int type, check
-		// to ensure that file is not larger than Integer.MAX_VALUE.
-		if (length > Integer.MAX_VALUE) {
-			// File is too large
-		}
-
-		// Create the byte array to hold the data
-		byte[] bytes = new byte[(int) length];
-
-		// Read in the bytes
-		int offset = 0;
-		int numRead = 0;
-		while (offset < bytes.length
-				&& (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
-			offset += numRead;
-		}
-
-		// Ensure all the bytes have been read in
-		if (offset < bytes.length) {
-			throw new IOException("Could not completely read file "
-					+ file.getName());
-		}
-
-		// Close the input stream and return bytes
-		is.close();
-		return bytes;
-	}
-
+	/**
+	 * @param length is length of file
+	 * @param inStream input stream of input file
+	 * @return array of byte representing a input file
+	 */
 	private static byte[] getBytes(int length, InputStream inStream) {
 
 		int bytesRead = 0;
