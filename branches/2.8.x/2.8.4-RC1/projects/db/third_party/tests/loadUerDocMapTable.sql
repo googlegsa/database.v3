3dc496b7acefc46d9d935e8a50e525ee96b10883
sveldurthi@google.com <sveldurthi@google.com@be54feb2-af79-11de-9f64-4bfc55a448e5>
2012-01-20 02:14:02 +0000

    Deleting 2.8.4-RC1 release candidate branch

--- branches/2.8.x/2.8.4-RC1/projects/db/third_party/tests/loadUerDocMapTable.sql
+++
@@ -1,5 +0,0 @@
--- Creating Test Data
-INSERT INTO  user_doc_map(id, ReportId , username)  VALUES(1 , 1 , 'user1');
-INSERT INTO  user_doc_map(id, ReportId , username)  VALUES(2 , 2 , 'user2' );
-INSERT INTO  user_doc_map(id, ReportId , username)  VALUES(3 , 3 , 'user1');
-INSERT INTO  user_doc_map(id, ReportId , username)  VALUES(4 , 4 , 'user4');
