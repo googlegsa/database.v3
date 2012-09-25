b615d9bc0cffe776372990c0a42f493fe5f8f7df
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2012-09-25 01:07:09 +0000

    Fix Java7 compiler warning.  Code Review: http://codereview.appspot.com/6568044

--- projects/db/source/java/com/google/enterprise/connector/db/DBMain.java
+++ projects/db/source/java/com/google/enterprise/connector/db/DBMain.java
@@ -30,7 +30,7 @@ public class DBMain {
   public static void main(String[] args) throws Exception {
     // From our class, get the jar file URL to this class file, and
     // make our way to the the Manifest located in that jar file.
-    Class thisClass = DBMain.class;
+    Class<?> thisClass = DBMain.class;
     String resName = "/" + thisClass.getName().replace('.', '/') + ".class";
 
     // Locate the Jar file containing our class.
